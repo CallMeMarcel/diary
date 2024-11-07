4d3ec27c5ac77dd3b4c690a593d4f55c5252581f
CallMeMarcel <pedromhutagaol@gmail.com>
2024-11-08 00:22:45 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:buku_diary/Authtentication/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.dark,
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 159, 10, 154)),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
